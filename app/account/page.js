d43ee0b02f317b2655ca483f511248b195f09b2b
joel <olugaabiola18@gmail.com>
2024-09-28 03:25:13 +0100

    adding server action

--- app/account/page.js
+++ app/account/page.js
@@ -1,10 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
-export default function page() {
+export default async function page() {
+  const session = await auth();
+  // console.log(session);
+  const firstName = session.user.name.split(" ").at(0);
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome,Biola
+      Welcome, {firstName}
     </h2>
   );
 }
